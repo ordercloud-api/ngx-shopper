bf0811e03a372043e5b44932b759b94c5eac0bad
Oliver Heywood <oheywood@four51.com>
2018-06-27 11:08:46 -0500

    changing selector names to match pattern

--- src/UI/Buyer/src/app/shared/components/lineitem-list-wrapper/lineitem-list-wrapper.component.ts
+++ src/UI/Buyer/src/app/shared/components/lineitem-list-wrapper/lineitem-list-wrapper.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lineitem-list-wrapper',
+  selector: 'shared-lineitem-list-wrapper',
   templateUrl: './lineitem-list-wrapper.component.html',
   styleUrls: ['./lineitem-list-wrapper.component.scss']
 })
--- src/UI/Buyer/src/app/shared/components/toggle-favorite/toggle-favorite.component.ts
+++ src/UI/Buyer/src/app/shared/components/toggle-favorite/toggle-favorite.component.ts
@@ -3,7 +3,7 @@ import { faHeart as _faHeartFilled } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as _faHeartOutline } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
-  selector: 'toggle-favorite',
+  selector: 'shared-toggle-favorite',
   templateUrl: './toggle-favorite.component.html',
   styleUrls: ['./toggle-favorite.component.scss']
 })
