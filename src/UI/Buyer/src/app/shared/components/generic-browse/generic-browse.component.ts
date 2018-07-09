53f417e475b01033e55898f1a58861b0f9358bb8
Oliver Heywood <oheywood@four51.com>
2018-07-09 15:10:21 -0500

    remove dependency.

--- src/UI/Buyer/src/app/shared/components/generic-browse/generic-browse.component.ts
+++ src/UI/Buyer/src/app/shared/components/generic-browse/generic-browse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Meta } from '@ordercloud/angular-sdk';
 
 @Component({
