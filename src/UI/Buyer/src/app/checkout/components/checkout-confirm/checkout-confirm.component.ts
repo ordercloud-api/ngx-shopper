3f6d09b4c096f0c96e4d996cc7ca1f867572a920
Oliver Heywood <oheywood@four51.com>
2018-07-18 15:47:33 -0500

    Adding order comments.

--- src/UI/Buyer/src/app/checkout/components/checkout-confirm/checkout-confirm.component.ts
+++ src/UI/Buyer/src/app/checkout/components/checkout-confirm/checkout-confirm.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { CheckoutSectionBaseComponent } from '@app/checkout/components/checkout-section-base/checkout-section-base.component';
 import { AppStateService, OcLineItemService } from '@app/shared';
-import { Order, ListPayment, ListLineItem } from '../../../../../node_modules/@ordercloud/angular-sdk';
-import { Observable } from '../../../../../node_modules/rxjs';
+import { Order, ListPayment, ListLineItem, OrderService } from '@ordercloud/angular-sdk';
+import { Observable } from 'rxjs';
 import { AppPaymentService } from '@app/shared/services/app-payment-service/app-payment.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { applicationConfiguration, AppConfig } from '@app/config/app.config';
 
 @Component({
   selector: 'checkout-confirm',
@@ -11,6 +13,7 @@ import { AppPaymentService } from '@app/shared/services/app-payment-service/app-
   styleUrls: ['./checkout-confirm.component.scss']
 })
 export class CheckoutConfirmComponent extends CheckoutSectionBaseComponent implements OnInit {
+  form: FormGroup;
   order: Order;
   payments$: Observable<ListPayment>;
   lineItems$: Observable<ListLineItem>;
@@ -18,15 +21,29 @@ export class CheckoutConfirmComponent extends CheckoutSectionBaseComponent imple
   constructor(
     private appStateService: AppStateService,
     private appPaymentService: AppPaymentService,
-    private ocLineItemService: OcLineItemService
+    private ocLineItemService: OcLineItemService,
+    private formBuilder: FormBuilder,
+    private orderService: OrderService,
+    @Inject(applicationConfiguration) private appConfig: AppConfig
   ) {
     super();
   }
 
   ngOnInit() {
+    if (!this.appConfig.anonymousShoppingEnabled) {
+      this.form = this.formBuilder.group({ comments: ''});
+    }
     this.order = this.appStateService.orderSubject.value;
     this.payments$ = this.appPaymentService.getPayments('outgoing', this.order.ID);
     this.lineItems$ = this.ocLineItemService.listAll(this.order.ID);
+  }
+
+  saveComments() {
+    this.order.Comments = this.form.get('comments').value;
+    this.orderService.Patch('outgoing', this.order.ID, this.order).subscribe(order => {
+      this.appStateService.orderSubject.next(order);
+      this.continue.emit();
+    });
   }
 
 }
