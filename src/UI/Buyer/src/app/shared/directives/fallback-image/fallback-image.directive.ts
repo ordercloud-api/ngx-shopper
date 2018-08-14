38e303972740745c1ff23cd9e0a94b5f086c8be7
Crhistian Ramirez <cramirez@four51.com>
2018-08-13 21:49:51 -0500

    add fallback image directive
    this directive will listen to image error event and replace the failed image with a fallback one

---
+++ src/UI/Buyer/src/app/shared/directives/fallback-image/fallback-image.directive.ts
@@ -0,0 +1,49 @@
+import { Directive, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
+@Directive({
+  selector: 'img[appFallbackImage]'
+})
+export class FallbackImageDirective implements OnInit, OnDestroy {
+
+  private alive = true;
+  private hasUpdatedImg = false;
+
+  // accepts image url to replace failed one with
+  // if none is provided will use default set here
+  @Input('appFallbackImage') appFallbackImage = 'http://placehold.it/300x300';
+
+  constructor(
+    private elementRef: ElementRef
+  ) { }
+
+  ngOnInit() {
+
+    // listen to error event on element
+    fromEvent(this.elementRef.nativeElement, 'error')
+      .pipe(
+
+        // only subscribe to event while directive
+        // is alive to prevent memory leak
+        takeWhile(() => this.alive)
+      )
+      .subscribe(() => {
+
+        // prevent potential infinite loop in case
+        // fallback image errors as well
+        if (!this.hasUpdatedImg) {
+          this.hasUpdatedImg = true;
+          this.updateImageUrl();
+        }
+      });
+  }
+
+  private updateImageUrl() {
+    this.elementRef.nativeElement.src = this.appFallbackImage;
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+}
