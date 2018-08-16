26a9cc5fe64faebe921078f61a0987a3041e3373
Oliver Heywood <oheywood@four51.com>
2018-08-16 11:46:16 -0500

    Refactor to share search and pagination in wrapper component.

--- src/UI/Seller/src/app/shared/shared.module.ts
+++ src/UI/Seller/src/app/shared/shared.module.ts
@@ -20,6 +20,7 @@ import { ModalService } from '@app-seller/shared/services/modal/modal.service';
 import { ModalComponent } from '@app-seller/shared/components/modal/modal.component';
 import { AppFormErrorService } from '@app-seller/shared/services/form-error/form-error.service';
 import { CarouselSlideDisplayComponent } from '@app-seller/shared/components/carousel-slide-display/carousel-slide-display.component';
+import { GenericBrowseComponent } from './components/generic-browse/generic-browse.component';
 
 @NgModule({
   imports: [
@@ -51,12 +52,14 @@ import { CarouselSlideDisplayComponent } from '@app-seller/shared/components/car
     SortColumnComponent,
     ModalComponent,
     CarouselSlideDisplayComponent,
+    GenericBrowseComponent,
   ],
   declarations: [
     SearchComponent,
     SortColumnComponent,
     ModalComponent,
     CarouselSlideDisplayComponent,
+    GenericBrowseComponent,
   ],
 
   /**
