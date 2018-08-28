8515fd2958a78425ac9faeb4a60a3b5fc4c6efb6
Crhistian Ramirez <cramirez@four51.com>
2018-08-28 17:13:52 -0500

    add placeholder states for static content pages #171

---
+++ src/UI/Buyer/src/app/static-pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'static-terms-and-conditions',
+  templateUrl: './terms-and-conditions.component.html',
+  styleUrls: ['./terms-and-conditions.component.scss']
+})
+export class TermsAndConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
