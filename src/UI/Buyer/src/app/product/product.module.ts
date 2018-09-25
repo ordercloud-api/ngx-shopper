8f7c5ca6f55b09faa6181d4ace40d9c34cfd19fe
Oliver Heywood <oheywood@four51.com>
2018-09-25 14:08:11 -0500

    Swapping image zoom libraries for one that work better in IE.

--- src/UI/Buyer/src/app/product/product.module.ts
+++ src/UI/Buyer/src/app/product/product.module.ts
@@ -5,15 +5,20 @@ import { ProductsRoutingModule } from '@app-buyer/product/product-routing.module
 import { ProductListComponent } from '@app-buyer/product/containers/product-list/product-list.component';
 import { ProductDetailsComponent } from '@app-buyer/product/containers/product-details/product-details.component';
 import { AdditionalImageGalleryComponent } from '@app-buyer/product/components/additional-image-gallery/additional-image-gallery.component';
-import { ImageZoomModule } from 'angular2-image-zoom';
 import { PriceFilterComponent } from '@app-buyer/product/components/price-filter/price-filter.component';
 import { CategoryNavComponent } from '@app-buyer/product/components/category-nav/category-nav.component';
 import { SortFilterComponent } from '@app-buyer/product/components/sort-filter/sort-filter.component';
 import { FacetFilterComponent } from '@app-buyer/product/components/facet-filter/facet-filter.component';
 import { FacetListComponent } from '@app-buyer/product/components/facet-list/facet-list.component';
+import { NgxImageZoomModule } from 'ngx-image-zoom';
 
 @NgModule({
-  imports: [SharedModule, ProductsRoutingModule, ImageZoomModule, TreeModule],
+  imports: [
+    SharedModule,
+    ProductsRoutingModule,
+    TreeModule,
+    NgxImageZoomModule,
+  ],
   declarations: [
     ProductListComponent,
     ProductDetailsComponent,
