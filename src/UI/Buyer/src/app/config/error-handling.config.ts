fca669dd70366dd8ac380225eeb372b693b368ec
Crhistian Ramirez <cramirez@four51.com>
2018-11-12 09:23:55 -0600

    display pretty webhook errors to user

--- src/UI/Buyer/src/app/config/error-handling.config.ts
+++ src/UI/Buyer/src/app/config/error-handling.config.ts
@@ -24,10 +24,14 @@ export class AppErrorHandler extends ErrorHandler {
     let message = '';
     if (ex && ex.error && ex.error.Errors && ex.error.Errors.length) {
       const e = ex.error.Errors[0];
-      message =
-        e.ErrorCode === 'NotFound'
-          ? `${e.Data.ObjectType} ${e.Data.ObjectID} not found.`
-          : e.Message;
+      if (e.Data && e.Data.WebhookName) {
+        // webhook error
+        message = e.Data.body;
+      } else if (e.ErrorCode === 'NotFound') {
+        message = `${e.Data.ObjectType} ${e.Data.ObjectID} not found.`;
+      } else {
+        message = e.Message;
+      }
     } else if (ex && ex.error && ex.error['error_description']) {
       message = ex.error['error_description'];
     } else if (ex.error) {
